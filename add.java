8a754ed0b4c655acf076a0957a0e5ec103b143c6
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-04-08 19:53:23 -0300

    save

---
+++ add.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input two numbers
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        // Perform addition
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        scanner.close();
+    }
+}
